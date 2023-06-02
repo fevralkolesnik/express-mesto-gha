934dc41a23d9f764572121a2b8618609b48131ea
Daria Kolesnikova <fevralkolesnik@yandex.ru>
2023-06-02 16:34:28 +0300

    fix: исправлены ошибки ESLint, исправлены ошибки влидации

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
-.then(() => console.log('Connected'))
-.catch((err) => console.log(`Something goes wrong: ${err}`));
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use((req, res, next) => {
   req.user = {
@@ -22,6 +19,4 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
-});
+app.listen(PORT);
