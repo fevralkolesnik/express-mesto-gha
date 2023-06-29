e5f219776af500f122adac0cb8d95d45931fa76c
Daria Kolesnikova <fevralkolesnik@yandex.ru>
2023-06-29 12:18:56 +0300

    feat: cors, logger

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const { createUser, login } = require('./controllers/users');
 const { auth } = require('./middlewares/auth');
 const { errorHandler } = require('./middlewares/errorHandler');
 const { validationCreateUser, validationLogin } = require('./middlewares/joiValidation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -14,6 +16,16 @@ app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.use(cors);
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/users/signup', validationCreateUser, createUser);
 app.post('/users/signin', validationLogin, login);
 
@@ -21,6 +33,8 @@ app.use(auth);
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
+app.use(errorLogger);
 
 app.use((req, res, next) => {
   next(new DocumentNotFoundError('Данная страница не найдена'));
